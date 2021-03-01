c31f4941835fe4ae5b608df84903f590411746f7
martinagalletti <45358914+martinagalletti@users.noreply.github.com>
2021-03-01 11:54:14 +0100

    Update api_keys.lisp

--- api_keys.lisp
+++ api_keys.lisp
@@ -1,10 +1,10 @@
 (in-package :web-services)
 
 ;; Please replace these API keys with yours:
-(set-api-keys :google-knowledge-graph "AIzaSyDR9MVg0_Zh6QrKD3M7SzZqQJ9Tn9I7GxY"
-              :merriam-webster "8fe92f45-0f31-4ec1-8b3f-c11cb403d657"
-              :words "ef0b0b01fbmshe99d52e360999bcp116ad7jsn90dcbb775019"
-              :mediastack "357acde6d8d40889c97558fc6581649e"
-              :MealDB "1")
+(set-api-keys :google-knowledge-graph ""
+              :merriam-webster ""
+              :words ""
+              :mediastack ""
+              :MealDB "")
 
 ;; For information on how to obtain them, please see the "Getting your own API Keys" section at https://github.com/SonyCSLParis/web-services
